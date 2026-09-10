4280f1570d5a26f9228dd24eef337f5fdbec055c
me-agrim-rehan <agrimrehan.work@gmail.com>
2026-09-10 11:06:20 +0530

    sublime text set up done

--- prac.c
+++ prac.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-int main() {
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+//Code below this 
+// cmd + shift + b for save and run 
+  
+
     int x;
     scanf("%d",&x);
     (x%7==0) ? printf("%d" , (int)(x*4.0)) : printf("%0.4f",(x/3.0) ) ;
+
+
     return 0;
 }
